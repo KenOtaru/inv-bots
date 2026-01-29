1d241f0975f8482d32fc33c5e134d4a852dc2f79
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-01-29 07:20:40 +0100

    Updated

--- kclaude-fibo.js
+++ kclaude-fibo.js
@@ -661,7 +661,7 @@ class RelativeVolatilityEngine {
         const combinedDeviation = avgEntropyDev * this.CONCENTRATION_WEIGHT +
             (-avgStreakDev) * this.STREAK_WEIGHT;
 
-        console.log('Combined Deviation:', combinedDeviation);
+        // console.log('Combined Deviation:', combinedDeviation);
 
         // Determine level based on how much less random than expected
         let level;
