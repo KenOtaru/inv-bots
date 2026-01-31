d5045a2d7b31346ba467bb3608006cbfba454ecc
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-01-31 16:35:03 +0100

    Updated

--- fibo-traderGrok.js
+++ fibo-traderGrok.js
@@ -624,7 +624,8 @@ class AIWeightedEnsembleBot {
         this.volatilityLevel = this.getVolatilityLevel2(history);
         console.log(`[${asset}] Volatility: ${vol} Volatility2: ${this.volatilityLevel}`);
 
-        if (vol === 'ultra-low' && (this.volatilityLevel === 'medium' || this.volatilityLevel === 'low')) {
+        // if (vol === 'ultra-low' && (this.volatilityLevel === 'medium' || this.volatilityLevel === 'low')) {
+        if (vol === 'ultra-low') {
             // Only trade Fibonacci saturation in these regimes
             if (maxScore >= 20.0 && recent.includes(predictedDigit)) {
                 this.lastPrediction = predictedDigit;
@@ -812,11 +813,11 @@ class AIWeightedEnsembleBot {
             if (this.consecutiveLosses === 4) this.x4Losses++;
             if (this.consecutiveLosses === 5) this.x5Losses++;
 
-            // if (this.consecutiveLosses === 2) {
-            //     this.currentStake = this.config.initialStake;
-            // } else {
-            this.currentStake = Math.ceil(this.currentStake * this.config.multiplier * 100) / 100;
-            // }
+            if (this.consecutiveLosses === 2) {
+                this.currentStake = this.config.initialStake;
+            } else {
+                this.currentStake = Math.ceil(this.currentStake * this.config.multiplier * 100) / 100;
+            }
             // this.suspendAsset(asset);
         }
 
