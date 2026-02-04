f237f611758004959a7465e77bf1965f963ddb64
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-02-04 08:55:02 +0100

    Updated

--- qpr.js
+++ qpr.js
@@ -635,7 +635,7 @@ class QuantumPhaseReversalBot {
             if (this.ticks % 20 === 0) {
                 console.log(
                     `[${asset}] PHASE REJECT ` +
-                    `shift=${condPhaseShift} dom2Strong=${condDom2Strong}(${dominance2}|${cfg.minDominance2}) incOK=${condIncrease}(${dominanceIncrease.toFixed(1)}|${cfg.minDominanceIncrease})`
+                    `shift=${condPhaseShift} dom2Strong=${condDom2Strong}(${dominance2}|${cfg.minDominance2}) incOK=${condIncrease}(${dominanceIncrease.toFixed(2)}|${cfg.minDominanceIncrease})`
                 );
             }
             return;
