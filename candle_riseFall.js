8581eb0c169cd940a2c33f28d125e92363aee0cb
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-02-15 08:43:42 +0100

    Updated

--- candle_riseFall.js
+++ candle_riseFall.js
@@ -399,7 +399,7 @@ const CONFIG = {
     CANDLES_TO_LOAD: 50,
 
     // Trade Duration Settings
-    DURATION: 14,
+    DURATION: 16,
     DURATION_UNIT: 's', // t=ticks, s=seconds, m=minutes
 
     // Trade Settings
