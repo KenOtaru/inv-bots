cbda8cbfa8fc3a0168ab1dd897a6fa3683d72bd7
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-02-24 20:09:45 +0100

    Updated

--- nGhost2M.js
+++ nGhost2M.js
@@ -9,7 +9,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const path = require('path');
 
-const TOKEN = "0P94g4WdSrSrzir";
+const TOKEN = "DMylfkyce6VyZt7";
 const TELEGRAM_TOKEN = "8288121368:AAHYRb0Stk5dWUWN1iTYbdO3fyIEwIuZQR8";
 const CHAT_ID = "752497117";
 
@@ -670,7 +670,7 @@ class RomanianGhostUltimate {
             analysis_window: 5000,
             min_ticks_for_analysis: 50,
             repeat_threshold: 8,
-            repeat_confidence: 50,
+            repeat_confidence: 60,
 
             // Money management
             baseStake: 2.20,
--- nGhostM2.js
+++ nGhostM2.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const path = require('path');
 
 
-const TOKEN = "0P94g4WdSrSrzir";
+const TOKEN = "rgNedekYXvCaPeP";
 const TELEGRAM_TOKEN = "8288121368:AAHYRb0Stk5dWUWN1iTYbdO3fyIEwIuZQR8";
 const CHAT_ID = "752497117";
 
