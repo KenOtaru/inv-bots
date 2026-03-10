7b975d1e44bca8d519f7c15b663041dc274ec978
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-03-10 07:55:23 +0100

    Updated

--- stepIndex.js
+++ stepIndex.js
@@ -47,7 +47,7 @@ const DEFAULT_CONFIG = {
 // FILE PATHS
 // ══════════════════════════════════════════════════════════════════════════════
 
-const STATE_FILE          = path.join(__dirname, 'ST5-grid-state0003.json');
+const STATE_FILE          = path.join(__dirname, 'ST5-grid-state0004.json');
 const STATE_SAVE_INTERVAL = 5000;
 
 // ══════════════════════════════════════════════════════════════════════════════
--- v75s2.js
+++ v75s2.js
@@ -47,7 +47,7 @@ const DEFAULT_CONFIG = {
 // FILE PATHS
 // ══════════════════════════════════════════════════════════════════════════════
 
-const STATE_FILE          = path.join(__dirname, 'v75-grid-state0006.json');
+const STATE_FILE          = path.join(__dirname, 'v75-grid-state0007.json');
 const STATE_SAVE_INTERVAL = 5000;
 
 // ══════════════════════════════════════════════════════════════════════════════
