771d4bd1603aebb35a34ade727a3658daa20ad13
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-03-11 11:37:30 +0100

    Updated

--- stepIndex2.js
+++ stepIndex2.js
@@ -21,8 +21,8 @@ const DEFAULT_CONFIG = {
   apiToken: 'hsj0tA0XJoIzJG5',
   appId:    '1089',
 
-  symbol:        'stpRNG',
-  tickDuration:  3,
+  symbol:        'stpRNG2',
+  tickDuration:  1,
   initialStake:  0.35,
   investmentAmount: 100,
 
