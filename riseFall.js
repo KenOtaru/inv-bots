15bf52cb5bc162626b131af15a30d2fe36f5bbd3
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-03-12 14:19:13 +0100

    Updated

--- riseFall.js
+++ riseFall.js
@@ -830,13 +830,13 @@ class DerivBot {
         if (level === 0) {
             return Number(base.toFixed(2));
         } else if (level < 4) {
-            return Number((base * CONFIG.MARTINGALE_MULTIPLIER).toFixed(2));
+            return Number(Math.pow(CONFIG.MARTINGALE_MULTIPLIER, level).toFixed(2));
         } else if (level < 6) {
-            return Number((base * CONFIG.MARTINGALE_MULTIPLIER2).toFixed(2));
+            return Number(Math.pow(CONFIG.MARTINGALE_MULTIPLIER2, level).toFixed(2));
         } else if (level < 8) {
-            return Number((base * CONFIG.MARTINGALE_MULTIPLIER3).toFixed(2));
+            return Number(Math.pow(CONFIG.MARTINGALE_MULTIPLIER3, level).toFixed(2));
         } else {
-            return Number((base * CONFIG.MARTINGALE_MULTIPLIER4).toFixed(2));
+            return Number(Math.pow(CONFIG.MARTINGALE_MULTIPLIER4, level).toFixed(2));
         }
     }
 
