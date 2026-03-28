79df5a6e8b032bda535c06fb09fc9937ffc98798
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-03-28 18:20:48 +0100

    Updated

--- nliveMulti4.js
+++ nliveMulti4.js
@@ -2225,6 +2225,7 @@ class EnhancedAccumulatorBot {
         if (this.config.enablePatternRecognition) {
             const recentDigits = runLengths.slice(-5);
             const ngramPred = this.patternEngine.predictFromNgram(asset, recentDigits, 3);
+            console.log('Ngram Prediction:', ngramPred);
             if (ngramPred) {
                 // Higher probability of specific digit = more predictable = potentially favorable
                 predictions.pattern = {
