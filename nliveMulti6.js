6d5214d6872cc8bad17d4041d95ff4ac6c34adfc
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-03-28 13:57:21 +0100

    Updated

--- nliveMulti6.js
+++ nliveMulti6.js
@@ -7203,12 +7203,12 @@ class EnhancedAccumulatorBot {
             return;
         }
 
-        if (this.consecutiveLosses > 0) {
-            if (this.lastEnsemblePredictions?.pattern?.confidence < 0.55) {
-                console.log(`[${asset}] ⚠️ Trade blocked due to low or No Pattern Model confidence`);
-                return;
-            }
-        }
+        // if (this.consecutiveLosses > 0) {
+        //     if (this.lastEnsemblePredictions?.pattern?.confidence < 0.55) {
+        //         console.log(`[${asset}] ⚠️ Trade blocked due to low or No Pattern Model confidence`);
+        //         return;
+        //     }
+        // }
 
         const request = {
             buy: assetState.currentProposalId,
