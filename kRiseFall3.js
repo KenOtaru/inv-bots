a99eaa0e4145e901d1f369de5dc4634ac13c901d
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-04-02 15:13:34 +0100

    Updated

--- kRiseFall3.js
+++ kRiseFall3.js
@@ -1097,7 +1097,7 @@ const CONFIG = {
     MAX_CANDLES_STORED: 50,
     CANDLES_TO_LOAD: 50,
 
-    CANDLE_PATTERN_LOOKBACK: 6, // Number of previous candles to analyze for pattern detection (user configurable)
+    CANDLE_PATTERN_LOOKBACK: 8, // Number of previous candles to analyze for pattern detection (user configurable)
 
     // Default Trade Duration Settings (used if asset has no specific config)
     DURATION: 58,
