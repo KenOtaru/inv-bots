0adc4ce764c2fcfa6fd4056dc1abea8c7f446f5c
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-04-06 07:50:59 +0100

    Updated

--- nliveMulti5.js
+++ nliveMulti5.js
@@ -1233,6 +1233,8 @@ class AccumulatorBotV4 {
             }
             this.hourlyStats.wins++;
             if (this.assetMetrics[asset]) this.assetMetrics[asset].wins++;
+
+            this.assets = ['R_10', 'R_25', 'R_50', 'R_75', 'R_100', '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V']
         } else {
             this.totalLosses++;
             this.consecutiveLosses++;
