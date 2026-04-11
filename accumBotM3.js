6b461344203bd05389c938de323082135830470d
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-04-11 13:54:43 +0100

    Updated

--- accumBotM3.js
+++ accumBotM3.js
@@ -1690,7 +1690,7 @@ const bot = new EnhancedDerivTradingBot('Dz2V2KvRf4Uukt3', {
     takeProfit: 10000,
     growthRate: 0.05,
     takeProfitMultiplier: 0.05,
-    filterNum: 2,
+    filterNum: 4,
     assets: ['R_10', 'R_25', 'R_50', 'R_75', 'R_100'],
     telegramToken: '8356265372:AAF00emJPbomDw8JnmMEdVW5b7ISX9_WQjQ',
     telegramChatId: '752497117',
