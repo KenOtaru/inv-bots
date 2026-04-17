97067f47481cf7bd1d40967d99ddc365d46640e1
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-04-17 18:26:47 +0100

    Updated

--- accumDifferBot.js
+++ accumDifferBot.js
@@ -750,14 +750,15 @@ class DigitDifferBot {
         };
 
         this._sendTelegram(
-            `🎯 <b>DIFFER TRADE OPENED</b>\n\n`
-                `Asset: <b>${asset}</b>\n`
-                `Betting digit <b>${predictedDigit}</b> will NOT appear\n`
-                `Score: ${(analysis.overallScore * 100).toFixed(1)}%`
-                `Hot digit appeared ${analysis.hotDigitCount}x in last ${this.cfg.digitWindow} ticks (${analysis.hotDigitPct}%)`
-                `Frequency edge over 2nd: ${analysis.frequencyEdge}`
-                `Stake: $${this.currentStake.toFixed(2)}\n`
-                `Consecutive losses: ${this.consecutiveLosses}`
+            `🎯 <b>DIFFER TRADE OPENED</b>\n\n
+                Asset: <b>${asset}</b>\n
+                Betting digit <b>${predictedDigit}</b> will NOT appear\n
+                Score: ${(analysis.overallScore * 100).toFixed(1)}%
+                Hot digit appeared ${analysis.hotDigitCount}x in last ${this.cfg.digitWindow} ticks (${analysis.hotDigitPct}%)
+                Frequency edge over 2nd: ${analysis.frequencyEdge}
+                Stake: $${this.currentStake.toFixed(2)}\n
+                Consecutive losses: ${this.consecutiveLosses}
+            `
         );
 
         this.lastTradeTime[asset] = Date.now();
