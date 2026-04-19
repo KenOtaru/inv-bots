b3264d612ed09111e872da4df69b6c201d9b2738
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-04-19 09:50:55 +0100

    Updated

--- kriseFall.js
+++ kriseFall.js
@@ -1209,6 +1209,7 @@ function getAssetConfig(symbol) {
     };
 }
 
+// let ACTIVE_ASSETS = ['R_10', 'R_25', 'R_50', 'R_75', 'R_100', 'stpRNG', 'stpRNG2', 'stpRNG3', 'stpRNG4', 'stpRNG5'];
 let ACTIVE_ASSETS = ['R_10', 'R_25', 'R_50', 'R_75', 'R_100', '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V'];
 // let ACTIVE_ASSETS = ['R_10', 'R_25', 'R_50', 'R_75', 'R_100', '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V', 'stpRNG', 'stpRNG2', 'stpRNG3', 'stpRNG4', 'stpRNG5'];
 
