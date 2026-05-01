5770b8557559a11401f83546133f629488773870
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2026-05-01 09:17:06 +0100

    Updated

--- candlePatternRFm.js
+++ candlePatternRFm.js
@@ -21,7 +21,7 @@ const path = require('path');
 
 const CONFIG = {
   // API Settings
-  API_TOKEN: 'Dz2V2KvRf4Uukt3',
+  API_TOKEN: 'rgNedekYXvCaPeP',
   APP_ID: '1089',
   WS_URL: 'wss://ws.derivws.com/websockets/v3',
 
@@ -46,9 +46,10 @@ const CONFIG = {
 
 // Active Assets List
 const ACTIVE_ASSETS = [
-  'R_10', 'R_25', 'R_50', 'R_75', 'R_100',
-  '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V',
-  'stpRNG', 'stpRNG2', 'stpRNG3', 'stpRNG4', 'stpRNG5'
+  // 'R_10', 'R_25', 'R_50', 'R_75', 'R_100',
+  // '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V',
+  // 'stpRNG', 'stpRNG2', 'stpRNG3', 'stpRNG4', 'stpRNG5'
+  'stpRNG'
 ];
 
 // ══════════════════════════════════════════════════════════════════════════════
@@ -86,11 +87,11 @@ const DEFAULT_ASSET_CONFIG = {
   TAKE_PROFIT: 10000,
 
   // Pattern Analysis Settings
-  PATTERN_MIN_CONFIDENCE: 0.60,
+  PATTERN_MIN_CONFIDENCE: 0.53,
   MIN_AGREEMENT_RATIO_CONFIDENCE: 0.80,
   MIN_PATTERN_CONFIDENCE: 0.98,
   MIN_PATTERN_CONFIDENCE_STEP_RNG: 0.97,
-  PATTERN_LENGTHS: [3, 4, 5, 6, 7, 8],
+  PATTERN_LENGTHS: [3], //[3, 4, 5, 6, 7, 8]
   PATTERN_MIN_OCCURRENCES: 5,
   PATTERN_RECENCY_DECAY: 0.9990,
   PATTERN_DOJI_THRESHOLD: 0.00001
